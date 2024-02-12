b2ac8476578f0bf0a30be92ad6f19f7fb542936f
travis <travisjb0@gmail.com>
2024-02-12 06:19:57 -0800

    Finished meme creator application

--- src/js/general.js
+++ src/js/general.js
@@ -4,4 +4,6 @@ import '../css/styles.css';
 // if you're using features of bootstrap that use js, uncomment the next line
 // import bootstrap from "bootstrap";
 
-console.log('Inside the General JS File');
+console.log('Inside the General JS File. I just edited this');
+
+
